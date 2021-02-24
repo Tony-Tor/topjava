d0d91988916c9b3f15645a89bf6aac2c8d3c75f0
TonyTor <Tony.Tor2016@yandex.ru>
2021-02-25 02:30:34 +0400

    HW3 complete

--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringRunner;
-import ru.javawebinar.topjava.UserTestData;
+import ru.javawebinar.topjava.web.UserTestData;
 import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -17,10 +17,10 @@ import ru.javawebinar.topjava.util.exception.NotFoundException;
 import java.util.List;
 
 import static org.junit.Assert.assertThrows;
-import static ru.javawebinar.topjava.UserTestData.*;
+import static ru.javawebinar.topjava.web.UserTestData.*;
 
 @ContextConfiguration({
-        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-app-test.xml",
         "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringRunner.class)
